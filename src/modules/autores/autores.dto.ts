622ebabf304cf301f16fae81bbe988e70b35365c
alexandresica <alexandresicavalcante@gmail.com>
2026-05-05 22:38:56 -0300

    feat/aplicado validação com as bibliotecas class-validator e class-transformer no DTO da criação do autor

--- src/modules/autores/autores.dto.ts
+++ src/modules/autores/autores.dto.ts
@@ -1,5 +1,23 @@
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CriarAutordto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => value.trim())
   nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  @Transform(({ value }) => value.trim())
   email: string;
 }
 export class AtualizarAutordto {
