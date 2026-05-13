ad75486fc7ef8f249cd00822a39debb350f50193
alexandresica <alexandresicavalcante@gmail.com>
2026-05-12 22:47:55 -0300

    feat/tabela livros criada no banco de dados

---
+++ src/db/schemas/livros.ts
@@ -0,0 +1,21 @@
+import {
+  mssqlTable,
+  int,
+  varchar,
+  datetime,
+  bit,
+} from 'drizzle-orm/mssql-core';
+
+export const livrosTabela = mssqlTable('livros', {
+  id: int().primaryKey().identity(),
+  idAutor: int('id_autor')
+    .notNull()
+    .references(() => autoresTabela.id),
+  titulo: varchar('titulo', { length: 100 }).notNull(),
+  descricao: text('descricao').notNull(),
+  ativo: bit('ativo').notNull().default(true),
+  criadoEm: datetime('criado_em').notNull().defaultGetDate(),
+});
+
+export type Livro = typeof livrosTabela.$inferInsert;
+export type CriarLivro = typeof livrosTabela.$inferInsert;
--- src/modules/autores/autores.dto.ts
+++ src/modules/autores/autores.dto.ts
@@ -5,6 +5,7 @@ import {
   IsString,
   MaxLength,
   MinLength,
+  IsOptional,
 } from 'class-validator';
 
 export class CriarAutordto {
@@ -21,6 +22,24 @@ export class CriarAutordto {
   email: string;
 }
 export class AtualizarAutordto {
-  nome?: string;
-  email?: string;
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsOptional({ message: 'O nome é opcional' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => {
+    const valor = typeof value;
+
+    if (valor === 'string') {
+      return value.trim();
+    }
+
+    return value;
+  })
+  nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  @Transform(({ value }) => value.trim())
+  email: string;
 }
