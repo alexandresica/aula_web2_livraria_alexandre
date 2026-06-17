c2bb0a776e46b529a116414b9db6c804b2a7f16a
alexandresica <alexandresicavalcante@gmail.com>
2026-06-16 22:48:38 -0300

    feat/novo módulo de usuários criado, incluindo controller, service, repository e tabela no banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AutoresModule } from './modules/autores/autores.module';
 import { DatabaseModule } from './db/database/database.module';
 import { LivrosModule } from './modules/livros/livros.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
 
 @Module({
-  imports: [AutoresModule, DatabaseModule, LivrosModule],
+  imports: [AutoresModule, DatabaseModule, LivrosModule, UsuariosModule],
   controllers: [],
   providers: [],
 })
---
+++ src/db/schemas/usuarios.ts
@@ -0,0 +1,19 @@
+import {
+  mssqlTable,
+  int,
+  varchar,
+  datetime,
+  bit,
+} from 'drizzle-orm/mssql-core';
+
+export const usuariosTabela = mssqlTable('usuarios', {
+  id: int().primaryKey().identity(),
+  nome: varchar('nome', { length: 100 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  passwordHashed: varchar('password_hashed', { length: 255 }).notNull(),
+  ativo: bit('ativo').notNull().default(true),
+  criadoEm: datetime('criado_em').notNull().defaultGetDate(),
+});
+
+export type Usuario = typeof usuariosTabela.$inferSelect;
+export type CriarUsuario = typeof usuariosTabela.$inferInsert;
